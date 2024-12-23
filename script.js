660fe5616ca69c2f68ae4072e97c9ec432a4110f
Kurtstephens3 <kurtstephens3@gmail.com>
2024-12-23 20:46:22 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript for interaction (contact button click event)
+document.getElementById('contactButton').addEventListener('click', function() {
+    alert('Thanks for reaching out! We will get back to you soon.');
+});
